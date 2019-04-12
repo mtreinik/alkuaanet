580ef4dc5b8fc9f15e492bba1f76ce502aee9cb6
Mikko Reinikainen <mikko.reinikainen@iki.fi>
2019-04-12 23:09:37 +0300

    give visual feedback when sound plays

--- src/Logo.js
+++ src/Logo.js
@@ -1,27 +1,30 @@
 import React from 'react';
-import Dialog from '@material-ui/core/Dialog';
 import Button from '@material-ui/core/Button';
-import Slide from '@material-ui/core/Slide';
-import Icon from '@material-ui/core/Icon';
-import Fab from '@material-ui/core/Fab';
 import logo from './img/aanirauta-32.png';
-import tuningFork from './img/aanirauta.png';
+import { withStyles } from '@material-ui/core/styles';
 
-const tuningForkStyle = {
-  maxWidth: "95%",
-  display: "block",
-  margin: "auto"
-}
-
-function Transition(props) {
-  return <Slide direction="down" {...props} />;
-}
+const styles = {
+  playing: {
+    width: 40,
+    height: 'auto',
+    transition: 'width 0.5s',
+  },
+  stopped: {
+    width: 32,
+    height: 'auto',
+    transition: 'width 0.5s'
+  }
+};
 
 class Logo extends React.Component {
-  state = {
-    open: false,
+  constructor(props) {
+    super(props);
+    this.state = {
+      open: false,
+      playing: false
 //    length: 0
-  };
+    };
+  }
   /*
   handleClickOpen = () => {
     this.props.noteAudioPlayer.playNote('A4');
@@ -34,7 +37,7 @@ class Logo extends React.Component {
   };
 
   playNote = () => {
-    this.props.noteAudioPlayer.playNote('A4');
+    this.props.noteAudioPlayer.playNotes('A4', this.startPlayback, this.endPlayback);
 //    this.setState({length: this.state.length + 1});
   }
 /*
@@ -53,33 +56,35 @@ class Logo extends React.Component {
     window.removeEventListener('devicemotion', this.handleDeviceMotion, true);
   }
 */
+
+  startPlayback = () => {
+    console.log('playing');
+    this.setState({
+      playing: true
+    })
+  }
+  endPlayback = () => {
+    console.log('stopping');
+    this.setState({
+      playing: false
+    })
+  }
+
   render () {
 //    window.addEventListener('devicemotion', this.handleDeviceMotion);
-
+    const { classes } = this.props;
     return <div>
-      <Button onClick={this.playNote}>
+      <Button onClick={this.playNote}
+      >
         <img
           src={logo}
           alt="Äänirauta"
+          className={ this.state.playing ? classes.playing : classes.stopped }
           />
       </Button>
-      <Dialog
-        fullScreen
-        open={this.state.open}
-        onClose={this.handleClose}
-        TransitionComponent={Transition}>
-        <Fab onClick={this.handleClose} style={{margin: "0.5em"}}>
-          <Icon>close</Icon>
-        </Fab>
-        <img
-          src={tuningFork}
-          alt="Äänirauta"
-          style={tuningForkStyle}
-          onClick={this.playNote}
-          />
-      </Dialog>
+
     </div>
   }
 }
 
-export default Logo;
+export default withStyles(styles)(Logo);
