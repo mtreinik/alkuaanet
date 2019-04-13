94949bdea92ebdbc2f9767b018873af26fc67536
Mikko Reinikainen <mikko.reinikainen@iki.fi>
2019-04-13 10:09:55 +0300

    incomplete implementation for opening/closing item

--- src/SongItem.js
+++ src/SongItem.js
@@ -2,17 +2,22 @@ import React from 'react';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
+import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
 import Icon from '@material-ui/core/Icon';
 import { withStyles } from '@material-ui/core/styles';
 
 const styles = {
+  notes: {
+    marginRight: "2em"
+  },
+  expand: {
+    margin: "0.5em"
+  },
   playing: {
-    marginLeft: "1em",
     transform: "scale(1.8)",
     transition: "transform 0.5s"
   },
   stopped: {
-    marginLeft: "1em",
     transform: "scale(1)",
     transition: "transform 0.5s"
   }
@@ -39,23 +44,36 @@ class SongItem extends React.Component {
       playing: false
     })
   }
+  handleOpenClose = () => {
+    this.props.handleOpenClose(this.props.id);
+  }
+
   render () {
     const { classes } = this.props;
     const secondary = this.props.lyrics +
-      (this.props.composer ? ` – <em>säv. ${this.props.composer}</em>` : '') +
-      (this.props.poet ? `, <em>san. ${this.props.poet}</em>` : '');
+      (this.props.composer ? ` – säv. ${this.props.composer}` : '') +
+      (this.props.poet ? `, san. ${this.props.poet}` : '');
     return <ListItem
         button
+        alignItems="flex-start"
         onClick={this.playNotes(this.props.notes)}>
-      <ListItemText
-        button="true"
-        primary={this.props.title}
-        secondary={secondary}/>
-      {this.props.notes}
       <ListItemIcon
         className={ this.state.playing ? classes.playing : classes.stopped }>
         <Icon>music_note</Icon>
       </ListItemIcon>
+      <ListItemText
+        primary={this.props.title}
+        secondary={secondary}/>
+      <span className={classes.notes}>
+        {this.props.notes}
+      </span>
+      <ListItemSecondaryAction>
+        <Icon
+          className={classes.expand}
+          onClick={this.handleOpenClose}>
+          {this.props.open ? "expand_less" : "expand_more" }
+        </Icon>
+      </ListItemSecondaryAction>
     </ListItem>;
   }
 }
--- src/SongList.js
+++ src/SongList.js
@@ -3,21 +3,41 @@ import List from '@material-ui/core/List';
 import SongItem from './SongItem.js';
 import CircularProgress from '@material-ui/core/CircularProgress';
 
-const SongList = props =>
-  <div>
-    { props.loaded || (
-        <CircularProgress color="secondary" style={{margin: "1em"}}/>
-      )
-    }
-    <List>
-      {props.songs.map(song => (
-        <SongItem
-          key={song.id}
-          noteAudioPlayer={props.noteAudioPlayer}
-          {...song}
-          />
-      ))}
-    </List>
-  </div>
+class SongList extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      openItemId: null
+    };
+  }
+
+  handleOpenClose = (itemId) => {
+    this.setState({
+      openItemId: this.state.openItemId === itemId ? null : itemId
+    });
+  }
+
+  render () {
+
+    return <div>
+      { this.props.loaded || (
+          <CircularProgress color="secondary" style={{margin: "1em"}}/>
+        )
+      }
+      <List>
+        {this.props.songs.map(song => (
+          <SongItem
+            key={song.id}
+            id={song.id}
+            open={this.state.openItemId === song.id ? true : false}
+            handleOpenClose={this.handleOpenClose}
+            noteAudioPlayer={this.props.noteAudioPlayer}
+            {...song}
+            />
+        ))}
+      </List>
+    </div>;
+  }
+}
 
 export default SongList;
