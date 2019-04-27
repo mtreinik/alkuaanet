12c9de44ef1b1d56ea08aaf1e43f7bf8829c3f07
Mikko Reinikainen <mikko.reinikainen@iki.fi>
2019-04-27 22:02:05 +0300

    added trivial test cases

--- src/NoteUtils.test.ts
+++ src/NoteUtils.test.ts
@@ -1,6 +1,9 @@
 import NoteUtils from './NoteUtils';
 
 it('converts B to H correctly', () => {
+  expect(NoteUtils.convertBtoH('')).toBe('');
+  expect(NoteUtils.convertBtoH('G3-C3')).toBe('G3-C3');
+
   expect(NoteUtils.convertBtoH('B4')).toBe('H4');
   expect(NoteUtils.convertBtoH('Bb3')).toBe('B3');
   expect(NoteUtils.convertBtoH('D4-Bb3-F3-Bb2')).toBe('D4-B3-F3-B2');
@@ -8,6 +11,9 @@ it('converts B to H correctly', () => {
 });
 
 it('converts H to B correctly', () => {
+  expect(NoteUtils.convertHtoB('')).toBe('');
+  expect(NoteUtils.convertHtoB('G3-C3')).toBe('G3-C3');
+
   expect(NoteUtils.convertHtoB('H2')).toBe('B2');
   expect(NoteUtils.convertHtoB('B3')).toBe('Bb3');
   expect(NoteUtils.convertHtoB('H3-F#3-E3')).toBe('B3-F#3-E3');
